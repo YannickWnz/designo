a5da973be8d04b1d3668e983820d6f5208d5e0de
yannickwnz <yannickwnz@gmail.com>
2023-10-30 19:51:22 +0000

    navbar responsiveness

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,46 @@
+import {Link} from 'react-router-dom'
+import {HiMenuAlt3} from 'react-icons/hi'
+import {AiOutlineClose} from 'react-icons/ai'
+
 import './Navbar.scss'
+import { useState } from 'react'
+
 
 export const Navbar = () => {
+
+    const [menuIconState, setMenuIconState] = useState(true)
+
+
     return (
-        <div className="navbar"></div>
+        <div className="navbar">
+            <div className="navbar-container">
+                <div className="logo-wrapper">
+                    {/* <img src="/public/assets/shared/desktop/logo-light.png" alt="" /> */}
+                    <Link to='/' ><img src="../assets/shared/desktop/logo-dark.png" alt="" /></Link>
+                </div>
+                <div className={`nav-links-wrapper ${menuIconState ? '' : 'menu-state'} `}>
+                    <ul>
+                        <li><Link to='/company'>OUR COMPANY</Link></li>
+                        <li><Link to='/locations'>LOCATIONS</Link></li>
+                        <li><Link to='/contact'>CONTACT</Link></li>
+                    </ul>
+                </div>
+                <div className="nav-icons-wrapper">
+                    {menuIconState && 
+                    <HiMenuAlt3 
+                    className='menu-icon' 
+                    onClick={() => {
+                        setMenuIconState(false)
+                    }}  />}
+                    {!menuIconState && 
+                    <AiOutlineClose 
+                    className='close-menu-icon' 
+                    onClick={() => {
+                        setMenuIconState(true)
+                    }}
+                    />}
+                </div>
+            </div>
+        </div>
     )
 }
