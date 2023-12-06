0d45120f573ee24c1ec71f8880b1a4a3eda96593
yannickwnz <yannickwnz@gmail.com>
2023-12-06 14:34:19 +0000

    commit

--- src/Components/ViewProjects/ViewProjects.jsx
+++ src/Components/ViewProjects/ViewProjects.jsx
@@ -1,15 +1,23 @@
+import { Link } from 'react-router-dom'
+import { IoIosArrowForward } from "react-icons/io";
+
 import './ViewProjects.scss'
 
 export const ViewProjects = () => {
     return (
         <div className="view-projects">
             <div className="view-projects-container">
-                <div className="projects-cat-one">
-                    <div className="web-design"></div>
+                <div className="web-design">
+                    <h1>WEB DESIGN</h1>
+                    <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link>
                 </div>
-                <div className="projects-cat-two">
-                    <div className="app-design"></div>
-                    <div className="graphic-design"></div>
+                <div className="app-design">
+                    <h1>APP DESIGN</h1>
+                    <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link>
+                </div>
+                <div className="graphic-design">
+                    <h1>APP DESIGN</h1>
+                    <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link>
                 </div>
             </div>
         </div>
