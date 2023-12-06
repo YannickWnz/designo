a9aaa33ae27927d6cc02928888b0652ee5b1e24d
yannickwnz <yannickwnz@gmail.com>
2023-12-06 13:38:11 +0000

    hero section completed

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from '../Button/Button'
 import './HeroSection.scss'
 
 function HeroSection() {
@@ -8,7 +9,8 @@ function HeroSection() {
                 <div className="text-intro-section">
                     <h1>Award-winning custom designs and digital branding solutions</h1>
                     <p>With overe 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services</p>
-                    <button>LEARN MORE</button>
+                    {/* <button>LEARN MORE</button> */}
+                    <Button btnContent='LEARN MORE' />
                 </div>
                 <div className="hero-img-section">
                     <img src="../assets/home/desktop/image-hero-phone.png" alt="" />
