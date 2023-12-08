7950460bc03e19d25b7b83b170eda35364f4688c
yannickwnz <yannickwnz@gmail.com>
2023-12-08 11:48:56 +0000

    commit

--- src/Components/ViewProjects/ViewProjects.jsx
+++ src/Components/ViewProjects/ViewProjects.jsx
@@ -7,17 +7,27 @@ export const ViewProjects = () => {
     return (
         <div className="view-projects">
             <div className="view-projects-container">
-                <div className="web-design">
-                    <h1>WEB DESIGN</h1>
-                    <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link>
-                </div>
+                    <div className="web-design">
+                        <Link to=''>
+                                <h1>WEB DESIGN</h1>
+                                {/* <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link> */}
+                                <p>VIEW PROJECTS <span><IoIosArrowForward /></span></p>
+                                {/* <div className="hover"></div> */}
+                        </Link>
+                    </div>
                 <div className="app-design">
-                    <h1>APP DESIGN</h1>
-                    <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link>
+                    <Link>
+                        <h1>APP DESIGN</h1>
+                        <p>VIEW PROJECTS <span><IoIosArrowForward /></span></p>
+                        {/* <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link> */}
+                    </Link>
                 </div>
                 <div className="graphic-design">
-                    <h1>APP DESIGN</h1>
-                    <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link>
+                    <Link>
+                        <h1>GRAPHIC DESIGN</h1>
+                        <p>VIEW PROJECTS <span><IoIosArrowForward /></span></p>
+                        {/* <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link> */}
+                    </Link>
                 </div>
             </div>
         </div>
