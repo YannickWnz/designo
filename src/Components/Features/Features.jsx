932150cba9a4f9aec8577cbcd469dff17eca67a3
yannickwnz <yannickwnz@gmail.com>
2024-01-08 21:01:30 +0000

    feature section

---
+++ src/Components/Features/Features.jsx
@@ -0,0 +1,44 @@
+import './Feature.scss'
+
+export const Features = () => {
+    return (
+        <div className="features">
+            <img src="../assets/shared/desktop/bg-pattern-leaf.svg" className='leaf-pattern' alt="" />
+            <div className="features-container">
+                <div className="features-wrapper">
+                    <div className="feature-illustration">
+
+                        <img src="../assets/home/desktop/illustration-passionate.svg" alt="" />
+
+                        <img src="../assets/shared/desktop/bg-pattern-small-circle.svg" className='img-pattern' alt="" />
+
+                    </div>
+                    <div className="feature-contents">
+                        <h1>PASSIONATE</h1>
+                        <p>Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.</p>
+                    </div>
+                </div>
+                <div className="features-wrapper">
+                    <div className="feature-illustration">
+                        <img src="../assets/home/desktop/illustration-resourceful.svg" alt="" />
+                        <img src="../assets/shared/desktop/bg-pattern-small-circle.svg" className='img-pattern' alt="" />
+                    </div>
+                    <div className="feature-contents">
+                        <h1>RESOURCEFUL</h1>
+                        <p>Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.</p>
+                    </div>
+                </div>
+                <div className="features-wrapper">
+                    <div className="feature-illustration">
+                        <img src="../assets/home/desktop/illustration-friendly.svg" alt="" />
+                        <img src="../assets/shared/desktop/bg-pattern-small-circle.svg" className='img-pattern' alt="" />
+                    </div>
+                    <div className="feature-contents">
+                        <h1>FRIENDLY</h1>
+                        <p> We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Components/ViewProjects/ViewProjects.jsx
+++ src/Components/ViewProjects/ViewProjects.jsx
@@ -10,23 +10,19 @@ export const ViewProjects = () => {
                 <div className="web-design">
                     <Link to=''>
                             <h1>WEB DESIGN</h1>
-                            {/* <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link> */}
                             <p>VIEW PROJECTS <span><IoIosArrowForward /></span></p>
-                            {/* <div className="hover"></div> */}
                     </Link>
                 </div>
                 <div className="app-design">
                     <Link>
                         <h1>APP DESIGN</h1>
                         <p>VIEW PROJECTS <span><IoIosArrowForward /></span></p>
-                        {/* <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link> */}
                     </Link>
                 </div>
                 <div className="graphic-design">
                     <Link>
                         <h1>GRAPHIC DESIGN</h1>
                         <p>VIEW PROJECTS <span><IoIosArrowForward /></span></p>
-                        {/* <Link to=''>VIEW PROJECTS <span><IoIosArrowForward /></span></Link> */}
                     </Link>
                 </div>
             </div>
