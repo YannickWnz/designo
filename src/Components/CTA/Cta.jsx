2309d0ac6fa3e2ab5931c5b79e80290c057a5d58
yannickwnz <yannickwnz@gmail.com>
2024-01-09 16:34:20 +0000

    cta section complete

--- src/Components/CTA/Cta.jsx
+++ src/Components/CTA/Cta.jsx
@@ -1,9 +1,19 @@
+import { Button } from '../Button/Button'
 import './Cta.scss'
 
 export const Cta = () => {
     return (
         <section className="cta">
-            <div className="cta-container"></div>
+            <div className="cta-container">
+
+                <img src="../assets/shared/desktop/bg-pattern-three-circles.svg" className='three-circle-pattern' alt="" />
+
+                <div className="text-contents">
+                    <h1>Let's talk about your project</h1>
+                    <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
+                </div>
+                <Button btnContent='GET IN TOUCH' />
+            </div>
         </section>
     )
 }
