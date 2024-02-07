2a2f68620fa4a8913a05687d3664d16432914e1e
yannickwnz <yannickwnz@gmail.com>
2024-02-07 15:27:10 +0100

    a lil bit of refactoring wont kill

---
+++ src/Components/WCTalentTexts/WCTalentTexts.js
@@ -0,0 +1,9 @@
+
+export const title1 = 'World-Class Talent'
+export const textContent1 = 'We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.'
+export const textContent2 = 'Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.'
+
+export const title2 = 'The real deal'
+export const textContent3 = 'As strategic partners in our clients\' businesses, we are ready to take on any challenge on our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.'
+
+export const textContent4 = 'We are visual storytellers in appealling and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.'
