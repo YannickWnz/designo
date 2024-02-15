cd386f2c517bdddc95fda0532e65c1b4d9db7cb4
yannickwnz <yannickwnz@gmail.com>
2024-02-15 14:19:24 +0100

    location page complete

--- src/Components/LocationDetails/LocationDetails.jsx
+++ src/Components/LocationDetails/LocationDetails.jsx
@@ -1,27 +1,39 @@
 import './LocationDetails.scss'
 
-export const LocationDetails = ({}) => {
+export const LocationDetails = ({country, addressOne, addressTwo, phone, email, reversed, mapImg }) => {
     return (
         <section className="location-details">
             <div className="details-container">
-                <div className="details-wrapper">
+                {/* <div className="details-wrapper"> */}
+                <div className={` details-wrapper ${reversed ? 'reverse' : ''} `}>
                     <div className="details-contents">
                         <img src="../assets/shared/desktop/bg-pattern-two-circles.svg" alt="" />
-                        <h1>Canada</h1>
+                        {/* <h1>Canada</h1> */}
+                        <h1>{country}</h1>
                         <div className="contents-wrapper">
                             <div className="office-location">
                                 <h3>Designo Central Office</h3>
-                                <p>3886 Wellington Street</p>
-                                <p>Toronto Ontario M9C 3J5</p>
+                                {/* <p>3886 Wellington Street</p> */}
+                                <p>{addressOne}</p>
+                                <p>{addressTwo}</p>
+                                {/* <p>Toronto Ontario M9C 3J5</p> */}
                             </div>
                             <div className="contact">
                                 <h3>Contact</h3>
-                                <p>P : +1 234 567 890 </p>
-                                <p>M : contact@designo.co</p>
+                                {/* <p>P : +1 234 567 890 </p> */}
+                                <p>P : {phone}</p>
+                                {/* <p>M : {contact@designo.co}</p> */}
+                                {country == 'Canada' && <p>contact@designo.co</p> }
+                                {country == 'Australia' && <p>contact@designo.au</p> }
+                                {country == 'United Kingdom' && <p>contact@designo.uk</p> }
+                                {/* <p>M : {` ${country = 'Canada' ? 'contact@designo.co' : ''} `}</p> */}
                             </div>
                         </div>
                     </div>
-                    <div className="details-map"></div>
+                    <div className="details-map">
+                        {/* <img src="../assets/locations/desktop/image-map-canada.png" alt="" /> */}
+                        <img src={mapImg} alt="" />
+                    </div>
                 </div>
             </div>
         </section>
--- src/Pages/Locations/Locations.jsx
+++ src/Pages/Locations/Locations.jsx
@@ -4,7 +4,13 @@ import './Locations.scss'
 export const Locations = () => {
     return (
         <div className="locations">
-            <LocationDetails />
+
+            <LocationDetails country='Canada' addressOne='3886 Wellington Street' addressTwo='Toronto Ontario M9C 3J5' phone='+1 234 567 890' mapImg='../assets/locations/desktop/image-map-canada.png'  />
+
+            <LocationDetails country='Australia' addressOne='19 Balonne Street' addressTwo='New South Wales 2443' phone='(02) 6720 9092' reversed={true} mapImg='../assets/locations/desktop/image-map-australia.png'  />
+
+            <LocationDetails country='United Kingdom' addressOne='13 Colorado Way' addressTwo='Rhydy-fro SA8 9GA' phone='078 3115 1400' mapImg='../assets/locations/desktop/image-map-united-kingdom.png'  />
+
         </div>
     )
 }
