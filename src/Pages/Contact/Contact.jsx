e4362342969eb8557a3777cf18b1f8d0d6cfa478
yannickwnz <yannickwnz@gmail.com>
2024-02-27 12:10:42 +0100

    contact page large screen completed

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,6 +2,32 @@ import './Contact.scss'
 
 export const Contact = () => {
     return (
-        <div className="contact"></div>
+        <div className="contact">
+            <div className="contact-container">
+                <div className="intro">
+                    <img src="../assets/about/desktop/bg-pattern-hero-about-desktop.svg" className='bg-pattern' alt="" />
+                    <div className="text-intro">
+                        <h1>Contact Us</h1>
+                        <p>Ready to take it the next level? Let's talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that's relatable to your users, drop us a line.</p>
+                    </div>
+                    <div className="contact-form">
+                        <form action="" className="">
+                            {/* <p>form goes in here</p> */}
+                            <input type="text" placeholder='Name' />
+                            <br />
+                            <input type="email" placeholder='Email' />
+                            <br />
+                            <input type="text" placeholder='Phone' />
+                            <br />
+                            <textarea name="" placeholder='Your message' id="" cols="30" rows="10"></textarea>
+                            <br />
+                            <div className="submit-btn">
+                                <input type="submit" value='SUBMIT' />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
